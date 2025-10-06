ea9c042fa685a439d770a3be18dc243ec3bdb526
Eden Garza <edengarza@tamu.edu>
2025-10-06 01:06:39 -0500

    style changes for rust+git, starting line processing for tests

--- tester/src/main.rs
+++ tester/src/main.rs
@@ -1,73 +1,197 @@
-use std::fs::{self, File}; // for file manip
-use std::io::{BufReader, BufRead, Result, Lines}; // for the buffer itself
-use std::path::{Path, PathBuf}; // for filenames
+// +-----------+
+// |  Imports  |
+// +-----------+
+use std::fs::{
+    self,
+    File
+}; // for file manip
+use std::io::{
+    BufReader,
+    BufRead,
+    Result,
+    Lines
+}; // for the buffer itself
+use std::path::{
+    Path,
+    PathBuf
+}; // for filenames
+use std::env;
 
-// the file reading comes from the rust-lang docs
-// https://doc.rust-lang.org/stable/rust-by-example/std_misc/file/read_lines.html
-
-// helper function to return a line by line iterator of a file
-// P is a generic type constrained to AsRef<Path>, allowing strings or paths
-fn read_lines<P>(filename: P) -> Result<Lines<BufReader<File>>> where P: AsRef<Path>, {
-    // open the file, but this is wrapped in a result
-    // ? will attempt to unwrap and if not it quickly returns an err
-    let file = File::open(filename)?;
-
-    // return wrapped so that caller can match
-    // BufReader will buffer the file efficiently (less internal allocs)
-    // lines() converts the output into an interator by newlines
-    Ok(BufReader::<File>::new(file).lines())
+// +-----------+
+// |  Structs  |
+// +-----------+
+struct TestConfig {
+    id: u64,
+    key: u64,
+    plaintext: u64,
+    ciphertext: u64,
 }
 
-fn get_files_in_dir(dirpath: &Path) -> Result<Vec<PathBuf>> {
+// +-----------+
+// |  Helpers  |
+// +-----------+
+
+// split a string into its non-whitespace components
+fn tokenize_line(line: &str) -> Vec<String> {
+    // create the return object
+    let mut tokens: Vec<String> = Vec::new();
+
+    // iterate over each token and add to return
+    for token in line.split_whitespace() {
+        tokens.push(String::from(token));
+    }
+
+    tokens
+}
+
+// determine whether a given string is a valid dir
+fn path_exists_and_is_dir(path: &str) -> bool {
+    Path::new(path).is_dir()    
+}
+
+// function to check the given args
+fn validate_args(args: &Vec<String>) {
+    if args.len() == 1 {
+        panic!("Did not provide any path to explore!");
+    }
+    if args.len() > 2 {
+        panic!("Gave too many paths!");
+    }
+    if !path_exists_and_is_dir(&args[1]) {
+        panic!("The given path was not a valid directory!");
+    }
+}
+
+// enumerate files in a directory and return vector of string paths
+fn get_files_in_dir(dirpath: &Path) -> Vec<String> {
     // the vector to hold the valid paths I want
-    let mut files = Vec::new();
+    let mut files: Vec<String> = Vec::new();
     
     // use read_dir() to get an iterator over entries in a dir
-    if dirpath.is_dir() {
-        for entry in fs::read_dir(dirpath)? {
-            let entry = entry?;
-            let metadata = entry.metadata()?;
+    if let Ok(entries) = fs::read_dir(dirpath) {
+        for entry in entries.flatten() {
+            // get file information
+            let os_name = entry.file_name();
+            let name = os_name.to_string_lossy();
 
-            // act differently based on type
+            // get rid of annoying emacs files
+            let autosave: bool = name.starts_with("#") && name.ends_with("#");
+            let backup: bool = name.ends_with("~");
+            if autosave || backup {
+                continue;
+            }
+            
+            // only add to the vector if it's not another dir
+            let metadata = entry.metadata().expect("Could not access file metadata.");
             if !metadata.is_dir() {
-                files.push(entry.path());
+                files.push(name.to_string());
             }
         }
     }
 
     // return the vector
-    Ok(files)
+    files
 }
 
-// helper function to take an iterator of lines from a buffer and do actions per line
-fn process_lines(lines: Lines<BufReader<File>>) {
-    // consume the iterator which returns a Result<String, io::Error>
+// +--------------+
+// |  Core Logic  |
+// +--------------+
+
+// function to gather all lines in a buffer
+fn read_lines(filename: &PathBuf) -> Lines<BufReader<File>> {
+    // open the file, but this is wrapped in a result
+    let file = File::open(filename).expect("Could not open given path");
+
+    // return an iterator of the lines in the file
+    BufReader::<File>::new(file).lines()
+}
+
+// function to act on the file line by line
+fn process_lines(lines: Lines<BufReader<File>>) -> Vec<TestConfig> {
+    // dict to hold values
+    let mut tests: Vec<TestConfig> = Vec::new();
+
+    // vector to hold relevant values
+    let mut temp_hold: Vec<String> = Vec::new();
+    
+    // go over each line in the file we read
     // map_while() auto stops iterating when an error is encountered
-    for (idx, line) in lines.map_while(Result::ok).enumerate() {
-        // action on each line, already unwrapped as String
-        println!("line {}: {}", idx, line);
+    for line in lines.map_while(Result::ok) {
+        // skip empty strings
+        if line.is_empty() {
+            continue;
+        }
+        
+        // determine action based on how the line starts
+        if line.starts_with("COUNT") ||
+            line.starts_with("KEYs") ||
+            line.starts_with("PLAINTEXT") ||
+            line.starts_with("CIPHERTEXT") {
+                // when a known prefix, add the token
+                let tokens: Vec<String> = tokenize_line(&line);
+
+                // only push the value onto the temp hold
+                temp_hold.push(tokens[2].clone());
+            } else if line.starts_with("[") {
+                // identify the sections, idk if i'm doing anything with them at the moment
+                println!("Section: {}", line);
+            } else {
+                // if not a pre-established beginning, just skip
+                continue;
+            }
+
+        // when an entire case has been found, form the struct
+        if temp_hold.len() == 4 {
+            // take the values in temp hold and create a test config
+            let test_config = TestConfig {
+                id: u64::from_str_radix(&temp_hold[0], 16).unwrap(),
+                key: u64::from_str_radix(&temp_hold[1], 16).unwrap(),
+                plaintext: u64::from_str_radix(&temp_hold[2], 16).unwrap(),
+                ciphertext: u64::from_str_radix(&temp_hold[3], 16).unwrap(),
+            };
+
+            // clear the temp hold
+            temp_hold.clear();
+
+            // store the config
+            tests.push(test_config);
+        }
     }
+
+    // return the test parsed test configs
+    tests
 }
 
+// +--------+
+// |  Main  |
+// +--------+
+
+// core logic driver
 fn main() {
+    // ensure the given args are valid
+    let args: Vec<String> = env::args().collect();
+    validate_args(&args);
+    
     // path to explore
-    let dirpath: &Path = Path::new("test_dir");
+    let dirpath: &Path = Path::new(&args[1]);
 
-    // enumerate a directory and return values in Result<Vec<PathBuf>>
-    println!("Enumerating directory {}:", dirpath.display());
-    let files = get_files_in_dir(&dirpath)
-        .expect("Should be able to open dirpath");
-
-    // printing filepaths
+    // enumerate the files in given directory
+    let files = get_files_in_dir(&dirpath);
+    
+    // for each file present in the directory, get the lines and act on them
     for file in files {
-        println!("{}", file.display());
+        // full the path out
+        let full_path = dirpath.join(file);
         
         // read a file and return Result<> holding an iterator 
-        let lines = read_lines(file)
-            .expect("Could not open {file}");
-
+        let lines = read_lines(&full_path);
+        
         // perform some action on the lines, expecting iterator
-        process_lines(lines);
-        println!();
+        let tests: Vec<TestConfig> = process_lines(lines);
+
+        // sanity check prints of test configs
+        for test in tests {
+            println!("id: {}\nkey: {}\nplaintext: {}\nciphertext: {}\n", test.id, test.key, test.plaintext, test.ciphertext);
+        }
     }
 }
